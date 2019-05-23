c88383d26d5d1b256c1062651e1deb4a94e799b7
gcioran <gabriel.cioran@gmail.com>
2019-05-23 20:40:44 +0200

    changes project carousel

--- src/app/detailed-project/detailed-project.component.ts
+++ src/app/detailed-project/detailed-project.component.ts
@@ -2,24 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { ProjectsService } from '../projects.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-detailed-project',
   templateUrl: './detailed-project.component.html',
-  styleUrls: ['./detailed-project.component.scss']
+  styleUrls: ['./detailed-project.component.scss'],
+  providers: [NgbCarouselConfig]  
 })
 export class DetailedProjectComponent implements OnInit {
+  public selectedProject!: any;
 
   constructor(
     public projectCollection: ProjectsService,
     private route: ActivatedRoute,
+    public config: NgbCarouselConfig,
     private location: Location) {
+      config.interval = 0;
       this.projectCollection = projectCollection;
     }
 
   ngOnInit(): void {
-    const project = this.getProject();
-    console.log('111', project);
+    this.selectedProject = this.getProject();
+    console.log('111', this.selectedProject);
   }
 
   getProject() {
