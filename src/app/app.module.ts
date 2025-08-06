8a425268010f2bbb03b6b2957f8d8c9d59ff2065
Gabriel Cioran <gabriel.cioran@gmail.com>
2025-08-06 11:23:19 +0300

    adapt for gh page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ export class MyHammerConfig extends HammerGestureConfig  {
 }
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
